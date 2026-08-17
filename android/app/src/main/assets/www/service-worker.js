83a0b3b7571f8a2c56bb260acab2b8606bbaad76
charlesbackup2025-code <charlesbackup2025@gmail.com>
2026-08-17 18:20:54 -0300

    Mapa de trajeto abaixo dos enderecos

--- android/app/src/main/assets/www/service-worker.js
+++ android/app/src/main/assets/www/service-worker.js
@@ -1,2 +1,2 @@
-const CACHE='motoja-demo-v8';
+const CACHE='motoja-demo-v9';
 const A=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
